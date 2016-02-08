d651fa30e381325d3000a1d9bb58459f5311198a
DANic <mdan1@yandex.ru>
2016-02-08 15:31:42 +0300

    first commit

---
+++ vm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#список Vmid
+ssh esxi 'vim-cmd vmsvc/getallvms'
+
+#Список снапшотов конретного Vmid
+ssh esxi ''
+#создание снапшота
+ssh esxi ''
+#Удалить снапшот
+ssh esxi ''
+#Оставновить VM
+ssh esxi ''
+#Запустить VM
+ssh esxi ''
+#Перезагрузить VM
+ssh esxi ''
